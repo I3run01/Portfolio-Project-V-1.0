7333c2a419a58380496a5793bcfd74ca5a74fe76
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-08-22 20:56:54 -0300

    c

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,25 +1,33 @@
 import { useContext, useEffect, useState } from "react"
 import { Context } from "../../contexts/Context"
 import { SkillStyle } from "./style"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const MySkills = () => {
 
     const {state, dispatch} = useContext(Context)
     const [skillsText, setSkillsText] = useState<string>(state.language.language == 'English' ? 'Skills' : 'Habilidades')
 
+    const [levelbarOnOrOff, setlevelbarOnOrOff] = useState('levelbarOff')
+
     useEffect(() => {
         setSkillsText(state.language.language == 'English' ? 'Skills' : 'Habilidades')
     }, [state.language.language ])
 
+    useEffect(() => {
+        AOS.init({duration: 1500});
+      }, [])
+
     const skills = [
         //Level is 1 to 5
-        {name: 'TypeScript', Level: 4},
-        {name: 'React.JS', Level: 3},
-        {name: 'Node.JS', Level: 2},
-        {name: 'Python', Level: 3},
-        {name: 'AdonisJS', Level: 2},
+        {name: 'TypeScript', level: 4},
+        {name: 'React.JS', level: 3},
+        {name: 'Node.JS', level: 2},
+        {name: 'Python', level: 3},
+        {name: 'AdonisJS', level: 2},
         {name: 'PHP', level: 2},
-        {name: 'MuSQL', level: 2},
+        {name: 'MySQL', level: 2},
         {name: 'MongoBD', level: 2}
     ]
 
@@ -27,8 +35,19 @@ export const MySkills = () => {
         <SkillStyle
         id="Skills"
         theme={state.theme.status}
-        scColor={state.secColor.secColorName as string}>
+        scColor={state.secColor.secColorName as string}
+        onMouseEnter={()=>{setlevelbarOnOrOff('levelbarOn')}}
+        onMouseLeave={() => {setlevelbarOnOrOff('levelbarOff')}}>
             <h1>{skillsText}</h1>
+
+            { skills.map((item, index) => ( 
+            <div className={'MySkill'} >
+                <h2>{item.name}</h2> 
+                <div className="emptybar">
+                    <div className={`levelbar ${levelbarOnOrOff + item.level}`}></div>
+                </div>
+            </div> ))}
+
         </SkillStyle>        
     ) 
 }
--- src/components/Skills/style.ts
+++ src/components/Skills/style.ts
@@ -15,6 +15,7 @@ export const SkillStyle = styled.div<Props>`
     flex-direction: column;
     justify-content: center;
     text-align: center;
+
     
     h1 {
         font-family: 'Rubik Dirt', cursive;
@@ -23,6 +24,52 @@ export const SkillStyle = styled.div<Props>`
         font-size: 3em;
         filter: drop-shadow(1px 1px 1px ${props => props.scColor});
     }
+
+    h2 {
+        font-family: 'Rubik Dirt', cursive;
+        color: ${props => props.theme == 'dark' ? '#e6e6e6' : '#1c1c1c'};
+        font-weight: lighter;
+        font-size: 1.2em;
+        text-align: start;
+    }
+
+    .MySkill {
+        :hover > h2 {
+            color: ${props => props.scColor};
+        }
+    }
+
+    .levelbar {
+        width: 0;
+        height: 100%;
+        background-color: ${props => props.scColor};
+        transition: all 2s ease-in-out;
+    }
+
+
+    .levelbarOn1 {
+        width: calc(100% * (1 / 5));
+    }
+    .levelbarOn2 {
+        width: calc(100% * (2 / 5));
+    }
+    .levelbarOn3 {
+        width: calc(100% * (3 / 5));
+    }
+    .levelbarOn4 {
+        width: calc(100% * (4 / 5));
+    }
+    .levelbarOn5 {
+        width: calc(100% * (5 / 5));
+    }
+
+    .emptybar {
+        height: 10px;
+        background-color: ${props => props.theme == 'dark' ? '#e6e6e6' : '#1c1c1c'};
+        
+    }
+
+    
 
     @media (min-width: 1050px) {
         h1 {
