ff7626052162da568f96abe467769ef1a1cb2196
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-08-27 14:22:38 -0300

    c

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -40,14 +40,18 @@ export const Project = ({titleENGUSA, titlePTBR}:Props ) => {
 
     const [index, setIndex] = useState<number>(0)
 
-    const [projectName, setProjectName] = useState<string>(projects[index].EnglishTitle)
+    const [projectName, setProjectName] = useState<string>(state.language.language == 'English' ? projects[index].EnglishTitle : projects[index].PortugueseTitle)
+
+    const [ProjectText, setProjectText] = useState<string>(state.language.language == 'English' ? projects[index].EnglishText : projects[index].PortugueseText)
 
     useEffect(() => {
         if(state.language.language == 'English') {
           setTitle(titleENGUSA)
-          
+          setProjectName(projects[index].EnglishTitle)
+
         }else {
           setTitle(titlePTBR)
+          setProjectName(projects[index].PortugueseTitle)
         }
     }, [state.language.language])
 
@@ -80,8 +84,8 @@ export const Project = ({titleENGUSA, titlePTBR}:Props ) => {
               <img className={changeSlide} src={projects[index].Img} alt=""/>
             </div>
             <div id="text">
-              <h2>Project Name</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur sed voluptatibus consectetur nisi ut? Quas quo voluptates odio ipsam aliquam dolorem, consequuntur cumque, maiores odit placeat velit vel necessitatibus tenetur.</p></div>
+              <h2>{projectName}</h2>
+              <p>orem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur sed voluptatibus consectetur nisi ut? Quas quo voluptates odio ipsam aliquam dolorem, consequuntur cumque, maiores odit placeat velit vel necessitatibus tenetur.</p></div>
           </div>
         </ProjectStyle>
     )
