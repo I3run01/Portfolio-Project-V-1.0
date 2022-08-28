c712175b3dee81d0f3a6615fa66dc48e0dcdfd67
I3run01 <brunnooa.v@gmail.com>
2022-08-28 12:39:19 -0300

    c

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,6 +3,7 @@ import { Context } from "../../contexts/Context"
 import { ProjectStyle } from "./style"
 import { NextProjectIcon } from "./icons";
 import { BackProjectIcon } from "./icons";
+import { projects } from "./Projects";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -12,30 +13,7 @@ type Props = {
 }
 
 export const Project = ({titleENGUSA, titlePTBR}:Props ) => {
-        //Projects
-        const projects = [
-          {
-            EnglishTitle: 'BMI Project',
-            PortugueseTitle: 'Projeto de IMC',
-            EnglishText: 'This project was a challenge for b7web. In this project, I made a BMI calculator. You put your weight and height and the project will give you your BMI. If you are too slim, normal, or overweight.',
-            PortugueseText: 'Esse projeto foi um desafio do b7web, consiste em uma cálculadora de IMC, ela mostra se você está muito magro, normal ou com sobrepeso.',
-            Img: 'images/ProjectsImages/BMICalculator.png',
-            Deploy: 'https://i3run01bmiproject.netlify.app',
-            Github: 'https://github.com/I3run01/Projeto_Calculo_IMC'
-          },
-          {
-            EnglishTitle: 'Keyboard Project',
-            PortugueseTitle: 'Projeto teclado',
-            EnglishText: 'I made a responsive keyboard with just JavaScript, CSS and HTML. If the screen is desktop, the keyboard will have the layout of a desktop keyboard. The opposite is also true, if the screen is of a phone, the keypad will be a telephone keypad.',
-            PortugueseText: "Eu fiz um teclado responsivo apenas com JavaScript, CSS e HTML. Se a tela for de desktop, o teclado terá o layout de um teclado de desktop. O oposto também é verdadeiro, se a tela for de um          telefone, o teclado será de um teclado de telefone.",
-            Img: 'images/ProjectsImages/KeyboardProject.png',
-            Deploy: 'https://i3run01.github.io/Proj-Keyboard/',
-            Github: 'https://github.com/I3run01/Proj-Keyboard'
-          },
-          
-        ]
 
-    //Constants
     const {state, dispatch} = useContext(Context)
 
     const [title, setTitle] = useState<string>(state.language.language == 'English' ? 
