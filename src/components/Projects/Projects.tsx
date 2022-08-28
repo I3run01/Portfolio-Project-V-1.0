44c7a73818dce896c69834dc5c8dfa3614b0db74
I3run01 <brunnooa.v@gmail.com>
2022-08-28 12:43:08 -0300

    c

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -8,7 +8,7 @@ export const projects = [
         Deploy: 'https://i3run01bmiproject.netlify.app',
         Github: 'https://github.com/I3run01/Projeto_Calculo_IMC'
     },
-      {
+    {
         EnglishTitle: 'Keyboard Project',
         PortugueseTitle: 'Projeto teclado',
         EnglishText: 'I made a responsive keyboard with just JavaScript, CSS and HTML. If the screen is desktop, the keyboard will have the layout of a desktop keyboard. The opposite is also true, if the screen is of a phone, the keypad will be a telephone keypad.',
@@ -16,5 +16,5 @@ export const projects = [
         Img: 'images/ProjectsImages/KeyboardProject.png',
         Deploy: 'https://i3run01.github.io/Proj-Keyboard/',
         Github: 'https://github.com/I3run01/Proj-Keyboard'
-      },
+    },
 ]
